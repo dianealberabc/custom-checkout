695fb8bde5484ed813aea6221ab6ebe3b29154b4
dianealberabc <info@dianealber.com>
2022-04-07 15:26:33 +0530

    change

--- include/db/config.php
+++ include/db/config.php
@@ -5,9 +5,9 @@
 	// define("DB_USER", "uxibeuwqdewwbo");
 	// define("DB_PASSWORD", "d0a934ffaa9cf860620288968a1b678f3e3e11bfeeda47d2a86a764191db6679");
 
-	define("SHOPIFY_API_KEY", "e3676ad1f3f88550aef8f967ceb12587");
-	define("SHOPIFY_SECRET_KEY", "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");
-	define("SHOPIFY_URL", "https://order-additional-tag.herokuapp.com/install/");
-	define("APP_URL", "https://order-additional-tag.herokuapp.com/app/");
+	define("SHOPIFY_API_KEY", "e3676adfhbfbh");
+	define("SHOPIFY_SECRET_KEY", "shpss_dgddffhf1");
+	define("SHOPIFY_URL", "https://shop/install/");
+	define("APP_URL", "https://shop/app/");
 
 ?>
