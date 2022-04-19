8f58575b79b86ea5913de6145b49c62fbd248360
nickseth <99780914+nickmediacarry@users.noreply.github.com>
2022-04-19 17:37:25 +0530

    initial commit

--- index.php
+++ index.php
@@ -14,36 +14,36 @@
   $access_token = "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"; 
   define('API_SECRET_KEY', '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');
   
-  function verify_webhook($data, $hmac_header)
-  {
-    $calculated_hmac = base64_encode(hash_hmac('sha256', $data, API_SECRET_KEY, true));
-    return hash_equals($hmac_header, $calculated_hmac);
-  }
-  $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
-  $data = file_get_contents('php://input');
-  $verified = verify_webhook($data, $hmac_header);
-  error_log('Webhook verified: '.var_export($verified, true)); // Check error.log to see the result
+  // function verify_webhook($data, $hmac_header)
+  // {
+  //   $calculated_hmac = base64_encode(hash_hmac('sha256', $data, API_SECRET_KEY, true));
+  //   return hash_equals($hmac_header, $calculated_hmac);
+  // }
+  // $hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+  // $data = file_get_contents('php://input');
+  // $verified = verify_webhook($data, $hmac_header);
+  // error_log('Webhook verified: '.var_export($verified, true)); // Check error.log to see the result
 
-  $json = $data;
-  $json = json_decode($json, true);
-  $id = $json['id'];
-  $na_tags_value = $json['tags'];
-  $t = json_encode($json['note_attributes']);
-  $note_attributes = json_decode($t, true);
-  foreach ($note_attributes as $value) {
-    $name = $value['name'];
-    $v = $value['value'];
+  // $json = $data;
+  // $json = json_decode($json, true);
+  // $id = $json['id'];
+  // $na_tags_value = $json['tags'];
+  // $t = json_encode($json['note_attributes']);
+  // $note_attributes = json_decode($t, true);
+  // foreach ($note_attributes as $value) {
+  //   $name = $value['name'];
+  //   $v = $value['value'];
 
-    if($name == 'data tags') {
-      if (strpos($na_tags_value, ',')) {
-        if($v != '') {
-          $na_tags_value = $na_tags_value.','.$v;
-          break;
-        }
-      } else{
-        $na_tags_value = $v;
-      }
-    }
-  }
-  $Shopify->updateOrderTag($shop_url, $access_token, $id, $na_tags_value);
+  //   if($name == 'data tags') {
+  //     if (strpos($na_tags_value, ',')) {
+  //       if($v != '') {
+  //         $na_tags_value = $na_tags_value.','.$v;
+  //         break;
+  //       }
+  //     } else{
+  //       $na_tags_value = $v;
+  //     }
+  //   }
+  // }
+  // $Shopify->updateOrderTag($shop_url, $access_token, $id, $na_tags_value);
 ?>
