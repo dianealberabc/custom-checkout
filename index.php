09b283498b2c1122e2b23575b0ac50e42e728619
dianealberabc <info@dianealber.com>
2022-05-05 12:49:16 +0530

    new modify 5/5/022

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   $discount_data = json_decode($str_json);
   $discount_input = $discount_data->discount_input;
   $created_assets = $Shopify->checkDiscountNo("diane-alber.myshopify.com","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", $discount_input);
- echo json_encode($created_assets->errors);
+ echo json_encode(isset($created_assets->errors));
 //   if($created_assets->errors != "undefined"){
 //     echo $created_assets;
 //   } else{
