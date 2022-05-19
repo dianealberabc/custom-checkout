0bbb7d93a3cc3dd3aa2debd6c238da5304b2b7ca
dianealberabc <info@dianealber.com>
2022-05-19 16:06:21 +0530

    product file add 19/05/2022

--- products.php
+++ products.php
@@ -12,10 +12,10 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
    $str_json = file_get_contents('php://input');
    $pro_data = json_decode($str_json);
    $result = $Shopify->productData("diane-alber.myshopify.com","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", $pro_data->data);
-   if(isset($result->errors)){
+//    if(isset($result->errors)){
     echo json_encode($result);
-  } else{
-  echo json_encode($result); 
-  } 
+//   } else{
+//   echo json_encode($result); 
+//   } 
 
 ?>
