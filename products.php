4353f95902a6dcd9997c3fdc26a1f5a187699c70
dianealberabc <info@dianealber.com>
2022-05-19 16:32:24 +0530

    product file add 19/05/2022

--- products.php
+++ products.php
@@ -3,7 +3,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT');
     header('content-type: application/json; charset=utf-8');
-    require 'include/db/Store.php';
+    // require 'include/db/Store.php';
     include 'include/utils/Shopify.php';
     include 'include/utils/Tools.php';
 
